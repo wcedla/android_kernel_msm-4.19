4f23264d9240583c3cb575f4a9be577beab1f7ae
wcedla <wcedla@live.com>
2023-12-17 21:16:43 +0800

    Update drm_sysfs.c

--- drivers/gpu/drm/drm_sysfs.c
+++ drivers/gpu/drm/drm_sysfs.c
@@ -22,6 +22,7 @@
 #include <drm/drmP.h>
 #include "drm_internal.h"
 #include "drm_internal_mi.h"
+#include "../../../techpack/display/msm/sde/sde_connector.h"
 
 #define to_drm_minor(d) dev_get_drvdata(d)
 #define to_drm_connector(d) dev_get_drvdata(d)
