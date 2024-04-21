39f2633ce30e853a7ac5ce7e270469dc9a696ce8
wcedla <wcedla@live.com>
2024-04-21 13:52:11 +0800

    fix error

--- include/linux/fscrypt.h
+++ include/linux/fscrypt.h
@@ -36,7 +36,7 @@ struct fscrypt_name {
 	u32 hash;
 	u32 minor_hash;
 	struct fscrypt_str crypto_buf;
-	bool is_nokey_name;
+	bool is_ciphertext_name;
 };
 
 #define FSTR_INIT(n, l)		{ .name = n, .len = l }
